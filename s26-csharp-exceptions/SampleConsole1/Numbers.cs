192c53ec77ba32afd534e440374dfb2ddb03dd4b
msavarian <m-savarian@outlook.com>
2022-03-18 11:51:46 +0330

    inplement exception sample

---
+++ s26-csharp-exceptions/SampleConsole1/Numbers.cs
@@ -0,0 +1,45 @@
+﻿public class Numbers
+{
+    public int GrantParent(int a, int b)
+    {
+        Console.WriteLine("connection.open()");
+        var result = 0;
+        try
+        {
+
+            throw new FormatException("test format exception");
+
+            if (b == 0)
+                throw new ArgumentException("number can't divide by zero");
+
+            result = Parent(a, b);
+        }
+        catch (Exception ex)
+        {
+            //Console.WriteLine(" ----------- Divide - catch -----------------");
+            //Console.WriteLine(ex.Message);
+            //Console.WriteLine(ex.StackTrace);
+            throw; // new Exception("error with bad data", ex);
+        }
+        finally
+        {
+            Console.WriteLine("connection.close()");
+        }
+        return result;
+    }
+
+
+    public int Parent(int a, int b)
+    {
+        return Divide(a, b);
+    }
+
+    public int Divide(int a, int b)
+    {
+        var result = 0;
+
+        result = a / b;
+
+        return result;
+    }
+}
---
+++ s26-csharp-exceptions/SampleConsole1/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+
+try
+{
+    //var r1 = m1();
+    //Console.WriteLine("result = " + r1);
+
+    var r2 = m2();
+    Console.WriteLine("result = " + r2);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(" ----------- ArgumentException - catch -----------------");
+    Console.WriteLine("Error with parameters --- " + ex.Message);
+}
+catch (Exception ex)
+{
+    // log to database
+    // send an alert
+    // a good message
+
+    Console.WriteLine(" ----------- main - catch -----------------");
+    
+    Console.WriteLine(" ---------- Message -----------------");
+    Console.WriteLine(ex.Message);
+
+    Console.WriteLine(" ---------- StackTrace -----------------");
+    Console.WriteLine(ex.StackTrace);
+
+    var inner = ex.InnerException;
+    while (inner != null)
+    {
+
+        Console.WriteLine(" ---------- InnerException.Message -----------------");
+        Console.WriteLine(ex.InnerException.Message);
+        Console.WriteLine(" ---------- InnerException.StackTrace -----------------");
+        Console.WriteLine(ex.InnerException.StackTrace);
+        inner = inner.InnerException;
+    }
+
+    // throw;
+}
+
+
+int m1()
+{
+    Numbers demo = new Numbers();
+    return demo.GrantParent(6, 2);
+}
+
+
+
+int m2()
+{
+    Numbers demo = new Numbers();
+    return demo.GrantParent(5, 0);
+}
